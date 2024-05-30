533ff5990e8f51fbce403c5b3bce7b310bc04be3
TysonNW <tyson.vanover@gmail.com>
2024-05-30 12:04:12 -0700

    updated fixed powershell script

--- summarize.ps1
+++ summarize.ps1
@@ -1,14 +1,26 @@
-$basename = (Get-Item $args[0]).Basename
-$transcript = "$basename.text"
+$param = $args[0]
 
-./faster-whisper-xxl.exe $args[0] --model large-v2 --task transcribe -f text
+
+
+$basename = (Get-Item $param).Basename
+$targetpath = (Get-Item $param).DirectoryName
+$transcript = "$targetpath$basename.text"
+$summary = "$targetpath$basename-summary.txt"
+write-host $basename 
+write-host $targetpath 
+write-host $transcript 
+write-host $summary 
+
+
+& "C:\Program Files\Faster-Whisper-XXL\faster-whisper-xxl.exe" $param --model large-v2 --task transcribe -f text -o $targetpath
 
 If( Test-Path $transcript)
 {
-    Get-Content $transcript | llm -s "Summarize this text" -m mistral-7b-instruct-v0 > $basename-summary.txt
+    Get-Content $transcript | llm -s "Summarize this text" -m mistral-7b-instruct-v0 > $summary
     Rename-Item -Path $transcript -NewName "$basename.txt"
 }else
 {
     Write-Error "Faster-Whisper-XXL could not transcribe $s."
 }
 
+Read-Host -Prompt "Press Enter to exit"
